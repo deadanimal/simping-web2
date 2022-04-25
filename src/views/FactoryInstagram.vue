0544b34c3e2ad6646ececc985916729bf33a8e65
Afeez Aziz <afeezaziz@gmail.com>
2022-04-25 19:56:15 +0800

    asd

--- src/views/FactoryInstagram.vue
+++ src/views/FactoryInstagram.vue
@@ -11,7 +11,8 @@
         <div class="mt-4 flex md:mt-0 md:ml-4">
           <a href="https://api.instagram.com/oauth/authorize?client_id=298290545660406&redirect_uri=https://simping-api.onrender.com/instagram-redirect&scope=user_profile,user_media&response_type=code">
             <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Connect</button>
-          </a>                              
+          </a>     
+            <button type="button" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Create Collection</button>                                     
         </div>
     </div>        
       
