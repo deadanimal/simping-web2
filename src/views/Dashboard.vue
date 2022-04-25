db0255699cbcfbb4211e0a0b647a980e7cffea98
Afeez Aziz <afeezaziz@gmail.com>
2022-04-25 19:53:27 +0800

    dsa

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -58,7 +58,10 @@
 
     <div class="md:flex md:items-center md:justify-between py-4">
         <div class="flex-1 min-w-0">
-        <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Dashboard</h2>
+          <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Dashboard</h2>
+          <p>
+            <router-link to="/factory/instagram">Instagram</router-link> | 
+          </p>
         </div>
         <div class="mt-4 flex md:mt-0 md:ml-4">
           <router-link :to="profileUrl">
@@ -67,9 +70,9 @@
           <a :href="reloadUrl">
             <button type="button" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Reload</button>
           </a>    
-          <router-link to="/factory/instagram">
+          <!-- <router-link to="/factory/instagram">
             <button type="button" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">Instagram</button>
-          </router-link>                       
+          </router-link>                        -->
         </div>
     </div>
 
